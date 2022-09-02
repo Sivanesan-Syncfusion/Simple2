b192b268f1c188444c2532f4089ed2c7310f902a
SivanesanSyncfusion <92422799+SivanesanSyncfusion@users.noreply.github.com>
2022-09-02 09:58:56 +0530

    first-commit

---
+++ InstallApp.ps1
@@ -0,0 +1,3 @@
+﻿# Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
+# To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
+C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\ExploringAspNetCore\publish}
